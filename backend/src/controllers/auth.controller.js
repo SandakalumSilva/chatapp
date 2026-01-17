65618f63938a748309d98d9aec1d6ba90fb0cae3
SandakalumSilva <sandakalum1996@gmail.com>
2026-01-17 19:06:03 +0530

    welcome email send process done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,56 +1,64 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
-  const {fullName,email,password} = req.body;
+  const { fullName, email, password } = req.body;
 
-  try{
-    if(!fullName || !email || !password){
-      return res.status(400).json({msg:"Please fill all the fields"});
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ msg: "Please fill all the fields" });
     }
 
-    if(password.length < 6){
-      return res.status(400).json({msg:"Password must be at least 6 characters long"});
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ msg: "Password must be at least 6 characters long" });
     }
 
     //check if emails valid: regex
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailRegex.test(email)){
-      return res.status(400).json({msg:"Please enter a valid email"});
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ msg: "Please enter a valid email" });
     }
 
-    const user = await User.findOne({email});
-    if(user){
-      return res.status(400).json({msg:"Email already exists"});
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "Email already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       fullName,
       email,
-      password:hashedPassword
+      password: hashedPassword,
     });
-    
-    if(newUser){
-        generateToken(newUser._id,res);
-        await newUser.save();
 
-        return res.status(201).json({
-          _id:newUser._id,
-          fullName:newUser.fullName,
-          email:newUser.email,
-          profilePics:newUser.profilePics
-        });
-    }else{
-      return res.status(500).json({msg:"Invalid user data"});
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePics: newUser.profilePics,
+      });
+
+      try {
+        await sendWelcomeEmail(newUser.email, newUser.fullName, ENV.CLIENT_URL);
+      } catch (error) {
+        console.error("Error sending welcome email:", error);
+      }
+    } else {
+      return res.status(500).json({ msg: "Invalid user data" });
     }
-    
-   
-  }catch(error){
-    console.log("Error in signup",error);
-    return res.status(500).json({msg:"Internal server error"});
+  } catch (error) {
+    console.log("Error in signup", error);
+    return res.status(500).json({ msg: "Internal server error" });
   }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,26 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
-dotenv.config();
+
 const app = express();
 const __dirname = path.resolve();
 
-const port = process.env.PORT || 3000;
+const port = ENV.PORT || 3000;
 
 app.use(express.json());
 
-console.log(process.env.PORT);
+console.log(ENV.PORT);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 //make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
