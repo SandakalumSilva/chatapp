35af9dfa6b5fe15fdf54f29171695b23c20fc711
SandakalumSilva <sandakalum1996@gmail.com>
2026-01-18 17:25:34 +0530

    login & logout api's done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -62,3 +62,36 @@ export const signup = async (req, res) => {
     return res.status(500).json({ msg: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePics: user.profilePics,
+    });
+  } catch (error) {
+    console.log("Error in login", error);
+    return res.status(500).json({ msg: "Internal server error" });
+  }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ msg: "Logout successful" });
+};
